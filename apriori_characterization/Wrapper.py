6bd5c614584c424804bb40eaee65bb534d5fcaa0
Samuel <slefcourt12@gmail.com>
2020-04-20 01:43:55 -0500

    ..

--- apriori_characterization/Wrapper.py
+++ apriori_characterization/Wrapper.py
@@ -288,10 +288,10 @@ class Wrapper(object):
                     print('Anomaly')
 
                 # print statistics
-                running_loss += loss.item()
-                if i % 2000 == 1999:    # print every 2000 mini-batches
-                    print('[%d, %5d] loss: %.3f' %
-                          (epoch + 1, i + 1, running_loss / 2000))
+                # running_loss += loss.item()
+                # if i % 2000 == 1999:    # print every 2000 mini-batches
+                #     print('[%d, %5d] loss: %.3f' %
+                #           (epoch + 1, i + 1, running_loss / 2000))
 
         return rets, val_rets
 
