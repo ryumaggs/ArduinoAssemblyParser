aea79339ba463c2924c4185e391782bc354033b9
Samuel <slefcourt12@gmail.com>
2020-05-22 02:41:15 -0500

    ..

--- apriori_characterization/Wrapper.py
+++ apriori_characterization/Wrapper.py
@@ -395,8 +395,8 @@ class Wrapper(object):
         labels = []
 
 
-        print("Number testing files: ", len(data_loader.list_of_testing_files))
-        print("Number of training files: ", len(data_loader.list_of_training_files))
+        print("Number testing files: ", len(data_loader.dataset.list_of_testing_files))
+        print("Number of training files: ", len(data_loader.dataset.list_of_training_files))
         print("Number in data loader test: ", len(data_loader_test.num_files))
 
 
