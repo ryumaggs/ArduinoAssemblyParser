2a2e3dc50051417021ec55f765076405bad9f081
Samuel <slefcourt12@gmail.com>
2020-05-27 21:55:55 -0500

    hm

--- apriori_characterization/Main.py
+++ apriori_characterization/Main.py
@@ -83,7 +83,8 @@ def main(args):
         if args.ryu_testing:
             run_wrapper.ryu_test_procedure()
         else:
-            run_wrapper.test()
+            # run_wrapper.test()
+            run_wrapper.ryu_test_procedure()
     else:
         run_wrapper.train()
 
